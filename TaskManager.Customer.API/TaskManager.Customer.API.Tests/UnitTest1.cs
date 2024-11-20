88d098739722fe6660efdc0ce7b402c971addd1f
albert <albert1bennett@gmail.com>
2024-11-20 21:24:37 +0000

    Initial commit

---
+++ TaskManager.Customer.API/TaskManager.Customer.API.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace TaskManager.Customer.API.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
